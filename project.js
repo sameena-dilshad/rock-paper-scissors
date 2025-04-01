31041f25a51b55c98313e317162a2ac20701a3d8
sameena-dilshad <sameenask0212@gmail.com>
2025-04-01 17:05:41 +0530

    emojis not working

--- project.js
+++ project.js
@@ -4,27 +4,42 @@ const choices=document.querySelectorAll(".choice")//to store & get all div's
 const msg=document.querySelector("#msg");
 const userScorePara=document.querySelector("#user-score");
 const compScorePara=document.querySelector("#comp-score");
+const happyimg=document.querySelector("#happy");
+const sadimg=document.querySelector("#sad");
+const drawimg=document.querySelector("#draw");
 const getCompChoice=()=>{
     const options=["rock","paper","scissor"];//FROM string we cant select ,so we choose a array
     const randomIndx=Math.floor(Math.random()*3);
     return options[randomIndx];
 }
 const drawGame=()=>{
-    // console.log("game was draw.");
+    console.log("game was draw.");
+    happyimg.classList.add('hidden');
+    sadimg.classList.add('hidden');
+    drawimg.classList.remove('hidden');
+    drawimg.classList.add('visible');
     msg.innerText="game was draw.play again";
-    msg.style.backgroundColor="black";
+    
+   
 }
 const showWinner=(userWin,userChoice,CompChoice)=>{
+    drawimg.classList.add('hidden');
+    happyimg.classList.add('hidden');
+    sadimg.classList.add('hidden');
     if (userWin){
         userScore++;
         userScorePara.innerText=userScore;
-        // console.log("you win!");
+        happyimg.classList.remove('hidden');
+        happyimg.classList.add('visible');
+        console.log("you win!");
         msg.innerText="you win!";
         msg.style.backgroundColor="green";
     }else{
         compScore++;
         compScorePara.innerText=compScore;
-        // console.log("you lose");
+        sadimg.classList.remove('hidden');
+        sadimg.classList.add('visible');
+        console.log("you lose");
         msg.innerText="you lose";
         msg.style.backgroundColor="red";
     }
@@ -39,6 +54,7 @@ const playGame=(userChoice)=>{
         let userWin=true;
         if(userChoice==="rock"){
             userWin=CompChoice==="paper" ?false:true;
+            
         }else if(userChoice==="paper"){
             userWin=CompChoice==="scissors"?false:true;
         }else{
